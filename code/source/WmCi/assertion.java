8d3282357544b1da73ee62f7549769f96619d50f
Carlos Echeverría <CarlosEchev07@gmail.com>
2015-07-05 01:16:17 -0700

    Adding WmCi.assertion:notEqual java service assertion.

--- code/source/WmCi/assertion.java
+++ code/source/WmCi/assertion.java
@@ -1,7 +1,7 @@
 package WmCi;
 
 // -----( IS Java Code Template v1.2
-// -----( CREATED: 2015-07-04 23:54:52 MST
+// -----( CREATED: 2015-07-05 01:08:58 MST
 // -----( ON-HOST: 192.168.0.16
 
 import com.wm.data.*;
@@ -12,6 +12,7 @@ import com.wm.app.b2b.server.ServiceException;
 import java.util.ArrayList;
 import java.util.List;
 import com.wmci.assertion.*;
+import com.wmci.util.*;
 // --- <<IS-END-IMPORTS>> ---
 
 public final class assertion
@@ -34,28 +35,6 @@ public final class assertion
         throws ServiceException
 	{
 		// --- <<IS-START(equal)>> ---
-		// @specification WmCi.spec:BasicAssert
-		// @sigtype java 3.5
-		IDataCursor cursor = pipeline.getCursor();
-		String input = cursor.first("input") ? (String)cursor.getValue() : null;
-		String compare = cursor.first("compare") ? (String)cursor.getValue() : null;
-		
-		Result result = Eval.equal(input, compare);
-		
-		cursor.insertAfter("success", result.isSuccess());
-		cursor.insertAfter("message", result.getMessage());
-		cursor.destroy();
-		// --- <<IS-END>> ---
-
-                
-	}
-
-
-
-	public static final void equal2 (IData pipeline)
-        throws ServiceException
-	{
-		// --- <<IS-START(equal2)>> ---
 		// @specification WmCi.spec:BasicTestCase
 		// @sigtype java 3.5
 		IDataCursor pipeCursor = pipeline.getCursor();
@@ -64,41 +43,12 @@ public final class assertion
 			String input = pipeCursor.first("input") ? (String)pipeCursor.getValue() : null;
 			String compare = pipeCursor.first("compare") ? (String)pipeCursor.getValue() : null;
 			
-			String initTime = null;//yyyy/MM/dd hh:mm:ss:SSS
-			Result result = Eval.equal(input, compare);
-			String endTime = null;//yyyy/MM/dd hh:mm:ss:SSS
-		
-			// create an testDoc IData object
-			IData testDoc = IDataFactory.create();
-			// create a cursor to use to add key value tuples to the
-			// testDoc
-			IDataCursor testCaseResult = testDoc.getCursor();
-			// add key value tuples as required to the testDoc
-			testCaseResult.insertAfter("test", pipeCursor.first("test") ? (String)pipeCursor.getValue() : null);
-			testCaseResult.insertAfter("case", pipeCursor.first("case") ? (String)pipeCursor.getValue() : null);
-			testCaseResult.insertAfter("element", pipeCursor.first("element") ? (String)pipeCursor.getValue() : null);
-			testCaseResult.insertAfter("success", result.isSuccess());
-			testCaseResult.insertAfter("message", result.getMessage());
-			testCaseResult.insertAfter("initTime", initTime);
-			testCaseResult.insertAfter("endTime", endTime);
-			// ...
-			// destroy the intermediateCursor when done adding key value tuples
-			testCaseResult.destroy();
+			String initTime = Util.getTimeStamp();		
+			Result result = Eval.equal(input, compare);	
+			String endTime = Util.getTimeStamp();		
 			
-			// Take input resultList
-			IData[] outResultList;
-			IData[] inResultList = pipeCursor.first("resultList") ? IDataUtil.getIDataArray(pipeCursor, "resultList") : null;
-			
-			// Build new output resultList
-			if (inResultList == null) 
-				outResultList = new IData[1];
-			else {
-				outResultList = new IData[inResultList.length + 1];
-				System.arraycopy(inResultList, 0, outResultList, 0, inResultList.length);
-			}
-		
-			// Append new assertion
-			outResultList[outResultList.length - 1] = testDoc;
+			// Add test result to output document
+			IData[] outResultList = buildTestResults(pipeCursor, result, initTime, endTime);
 			
 			// add the outputDoc to the pipeline
 			IDataUtil.put(pipeCursor, "resultList", outResultList);
@@ -120,18 +70,73 @@ public final class assertion
 		// --- <<IS-START(notEqual)>> ---
 		// @specification WmCi.spec:BasicAssert
 		// @sigtype java 3.5
-		IDataCursor cursor = pipeline.getCursor();
-		String input = cursor.first("input") ? (String)cursor.getValue() : null;
-		String compare = cursor.first("compare") ? (String)cursor.getValue() : null;
+		IDataCursor pipeCursor = pipeline.getCursor();
 		
-		Result result = Eval.notEqual(input, compare);
-		
-		cursor.insertAfter("success", result.isSuccess());
-		cursor.insertAfter("message", result.getMessage());
-		cursor.destroy();
+		try {
+			String input = pipeCursor.first("input") ? (String)pipeCursor.getValue() : null;
+			String compare = pipeCursor.first("compare") ? (String)pipeCursor.getValue() : null;
+			
+			String initTime = Util.getTimeStamp();		
+			Result result = Eval.notEqual(input, compare);	
+			String endTime = Util.getTimeStamp();		
+			
+			// Add test result to output document
+			IData[] outResultList = buildTestResults(pipeCursor, result, initTime, endTime);
+			
+			// add the outputDoc to the pipeline
+			IDataUtil.put(pipeCursor, "resultList", outResultList);
+			
+		} finally {
+			// destroy the pipelineCursor
+			pipeCursor.destroy();
+		}
 		// --- <<IS-END>> ---
 
                 
 	}
+
+	// --- <<IS-START-SHARED>> ---
+	public static IData[] buildTestResults(IDataCursor pipeCursor, Result result, String initTime, String endTime) {
+		
+		IData testDoc = IDataFactory.create();
+		IDataCursor testCaseResult = testDoc.getCursor();
+		
+		// Build WmCi.docs:testCaseResult structure
+		//
+		if(pipeCursor.first("test"))
+			testCaseResult.insertAfter("test", (String)pipeCursor.getValue());
+		if(pipeCursor.first("case"))
+			testCaseResult.insertAfter("case", (String)pipeCursor.getValue());
+		if(pipeCursor.first("element"))
+			testCaseResult.insertAfter("element", (String)pipeCursor.getValue());
+		
+		testCaseResult.insertAfter("success", result.isSuccess());
+		
+		if(!Util.nvl(result.getMessage(), "", true).equals(""))
+			testCaseResult.insertAfter("message", result.getMessage());
+		
+		testCaseResult.insertAfter("initTime", initTime);
+		testCaseResult.insertAfter("endTime", endTime);
+		
+		testCaseResult.destroy();
+		
+		// Take input resultList
+		IData[] outResultList;
+		IData[] inResultList = pipeCursor.first("resultList") ? IDataUtil.getIDataArray(pipeCursor, "resultList") : null;
+		
+		// Build new output resultList
+		if (inResultList == null) 
+			outResultList = new IData[1];
+		else {
+			outResultList = new IData[inResultList.length + 1];
+			System.arraycopy(inResultList, 0, outResultList, 0, inResultList.length);
+		}
+	
+		// Append new assertion
+		outResultList[outResultList.length - 1] = testDoc;
+		
+		return outResultList;
+	}
+	// --- <<IS-END-SHARED>> ---
 }
 
